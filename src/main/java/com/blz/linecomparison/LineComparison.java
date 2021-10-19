e1e2ec3c5c9cc9311b8f74084bba5851330afcff
Saurabh Siddharth <saurabhsiddharth68>
2021-10-19 11:52:13 +0530

    Added:Welcome to Line Comparison Compution

---
+++ src/main/java/com/blz/linecomparison/LineComparison.java
@@ -0,0 +1,7 @@
+package com.blz.linecomparison;
+
+public class LineComparison {
+    public static void main(String[] args) {
+        System.out.println("Welcome To Line Comparison Computation Program");
+    }
+}
